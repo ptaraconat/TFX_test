4f1b6cfba11018430d902f3be8524c61312d3dc5
ptaraconat <104505251+ptaraconat@users.noreply.github.com>
2023-08-31 19:43:06 +0000

    add some comments in custom estimator

--- custom_estimators.py
+++ custom_estimators.py
@@ -12,15 +12,34 @@ class TFModel(tf.keras.Model):
     TensorFlow custom model 
     '''
     def __init__(self, 
-                 hidden_layers = [tf.keras.layers.Dense(units = 10, activation = 'relu'),
-                                  tf.keras.layers.Dense(units = 10, activation = 'relu')],
+                 hidden_layers = [tf.keras.layers.Dense(units = 10, 
+                                                        activation = 'relu'),
+                                  tf.keras.layers.Dense(units = 10, 
+                                                        activation = 'relu')],
                                   output_activation = 'linear'):
+        '''
+        Arguments : 
+        hidden_layers ::: list of keras.layers ::: Hidden layers of the NN 
+        output_activation ::: str ::: activation used for the 
+        prediction/last layer 
+        Return : 
+        None 
+        '''
         super(TFModel, self).__init__()
         self.hidden_layers = hidden_layers
         self.output_activation = output_activation
 
     def call(self,input):
+        '''
+        Call method. Calculate the forward pass in the NN 
+        Arguments : 
+        input ::: array like object [batch_size, input_shape] ::: examples for 
+        which the prediction will be made 
+        Return : 
+        x ::: array like object [batch_size, output_dim] ::: NN Prediction
+        '''
         x = input 
+        # loop over hidden_layers list 
         for layer in self.hidden_layers : 
             x = layer(x)
         x = self.output_layer(x)
@@ -28,26 +47,72 @@ class TFModel(tf.keras.Model):
     
     def set_output_dim(self, output_dim): 
         '''
-        Define the output layer given the output dimension. 
-        Will work only for flat output. 
-        Should ba called before building model 
+        Set model output layer given the output dimension. 
+        Will work only for flat output. Should ba called before building model. 
+        Arguments : 
+        output_dim ::: int ::: Dimension of the NN prediction 
+        Return : 
+        None 
         '''
         self.output_layer = tf.keras.layers.Dense(units = output_dim, 
                                                   activation = self.output_activation)
 
 class TFModelRegressor(TFModel): 
-
+    '''
+    Subclass of TFModel. 
+    Dedicated to Regression models. 
+    Output dim is only 1D, ATM. 
+    '''
     def __init__(self,
-                 hidden_layers = [tf.keras.layers.Dense(units = 10, activation = 'relu'),
-                                  tf.keras.layers.Dense(units = 10, activation = 'relu')]): 
+                 hidden_layers = [tf.keras.layers.Dense(units = 10, 
+                                                        activation = 'relu'),
+                                  tf.keras.layers.Dense(units = 10, 
+                                                        activation = 'relu')]): 
+        '''
+        Arguments : 
+        hidden_layers ::: list of keras.layers ::: Hidden layers of the NN 
+        Return : 
+        None
+        '''
         super(TFModelRegressor, self).__init__(hidden_layers= hidden_layers)
         self.output_activation = 'linear'
         self.set_output_dim(1)
+
+class TFModelClassifier(TFModel) : 
+    '''
+    Subclass of TFModel. 
+    Dedicated to Binary Classification models, ATM. 
+    '''
+    def __init__(self, 
+                 hidden_layers = [tf.keras.layers.Dense(units = 10, 
+                                                        activation = 'relu'),
+                                  tf.keras.layers.Dense(units = 10, 
+                                                        activation = 'relu')]):
+        super(TFModel, self).__init__(hidden_layers = hidden_layers)
+        '''
+        Arguments : 
+        hidden_layers ::: list of keras.layers ::: Hidden layers of the NN 
+        Return : 
+        None
+        '''
+        self.output_activation = 'sigmoid'
+        self.set_output_dim()
 
 class TFEstimator(BaseEstimator):
     ''' TensorFlow interfacer
     '''
     def __init__(self,model,optimizer,loss,batch_size = 200, epochs = 100):
+        '''
+        Arguments : 
+        model ::: TFModel object ::: Neural Network model 
+        optimizer ::: tf.keras.optimizer object::: optimizer used for updating NN 
+        weights
+        loss ::: tf.keras.loss object or str ::: loss function for computing model 
+        errors during training
+        batch_size ::: int ::: Sample size 
+        epochs ::: int ::: number of time the optimization iterates over the entire 
+        dataset
+        '''
 
         self.tf_model = model 
         self.optimizer = optimizer 
@@ -56,6 +121,14 @@ class TFEstimator(BaseEstimator):
         self.epochs = epochs
 
     def fit(self, X, y) : 
+        '''
+        Fit estimator to data 
+        Arguments : 
+        X ::: array like object [batch_size, input_shape] ::: Input data 
+        y ::: array like object [batch_size, output_dim] ::: Model targets 
+        Return : 
+        None  
+        '''
         self.tf_model.set_output_dim(y.shape[-1])
         self.tf_model.build(X.shape)
         self.tf_model.compile(optimizer = self.optimizer,loss = self.loss)
@@ -63,6 +136,13 @@ class TFEstimator(BaseEstimator):
         return self 
     
     def predict(self, X): 
+        '''
+        Arguments : 
+        X ::: array like object [batch_size, input_shape] ::: Input data 
+        Return : 
+        Predictions ::: array like object [batch_size, output_dim] ::: Model 
+        prediction
+        '''
         prediction = self.tf_model(X)
         return prediction.numpy()
     
@@ -70,7 +150,6 @@ class TFEstimator(BaseEstimator):
         return 0 
     
     def get_params(self, deep=True):
-        # suppose this estimator has parameters "alpha" and "recursive"
         return {"tf_model": self.tf_model, 
                 "optimizer": self.optimizer,
                 "loss": self.loss,
@@ -93,13 +172,33 @@ class TFEstimatorRegressor(TFEstimator):
         self.tf_model.output_layer = tf.keras.layers.Dense(units = 1, activation = 'linear')
     
     def fit(self, X, y, verbose = 0) : 
+        '''
+        Fit estimator to data 
+        Arguments : 
+        X ::: array like object [batch_size, input_shape] ::: Input data 
+        y ::: array like object [batch_size, output_dim] ::: Model targets 
+        verbose ::: int ::: 0 or 1 
+        Return : 
+        None  
+        '''
         self.tf_model.build(X.shape)
         self.tf_model.compile(optimizer = self.optimizer,loss = self.loss)
-        self.tf_model.fit(X, y, epochs = self.epochs, batch_size = self.batch_size, verbose =verbose)
+        self.tf_model.fit(X, y, epochs = self.epochs, batch_size = self.batch_size, 
+                          verbose =verbose)
         return self 
 
 def cross_validation(model_initializer, X, y, cross_val_approach, scoring): 
-
+    '''
+    Cross validation function. Implemented because sklearn cross_validate 
+    didn't work with the TFModel
+    Arguments : 
+    model_initializer ::: callable ::: Function that initialize the estimator 
+    X ::: array like object [batch_size, input_shape] ::: Model input 
+    y ::: array like object [batch_size, output_dim] ::: Model targets 
+    cross_val_approach ::: sklearn splitter object ::: Approach used for 
+    splitting data during cross validation 
+    scoring ::: dictionnary ::: dictionary of the scorer 
+    '''
     res_dict = dict()
     for key in scoring.keys(): 
         res_dict['train_'+key] = []
